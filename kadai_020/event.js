f4b02abcc6811d9c3d02148669a8eea697d0a236
maetel9 <saksi919192@gmail.com>
2025-06-26 09:13:08 +0900

    kadai020-1

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,6 @@
 const btn = document.getElementById('btn');
 
 btn.addEventListener('click', () => {
-  console.log('クリックされました！');
-
   const text = document.getElementById('text');
   text.textContent = 'ボタンをクリックしました';
 });
